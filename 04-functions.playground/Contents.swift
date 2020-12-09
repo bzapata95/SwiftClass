9460ce5543567c9b2ccf3c4811ba2cecacdee52b
Bryan Zapata <bmzc95@gmail.com>
2020-12-09 09:23:22 -0500

    class functions 2

--- 04-functions.playground/Contents.swift
+++ 04-functions.playground/Contents.swift
@@ -9,7 +9,7 @@ sayHelloWorld()
 // Parámetros de entrada
 func greeting(person: String, isMale: Bool) -> String {
     if isMale {
-        return "Bienvenido caballego \(person)"
+        return "Bienvenido caballero \(person)"
     } else {
         return "Bienvenido Señorita \(person)"
     }
@@ -20,3 +20,47 @@ greeting(person: "Olivia", isMale: false)
 
 // Parámetros de salida
 
+func greet2(person: String) {
+    print("Hola \(person)")
+}
+greet2(person: "Bryan")
+
+func printAndCount(string: String) -> Int {
+    print(string)
+    return string.count
+}
+
+
+func printWithoutCounting(string: String) {
+   let _ = printAndCount(string: string)
+}
+
+printAndCount(string: "Hola que tal")
+printWithoutCounting(string: "Hola que tal")
+
+func minMax(array: [Int]) -> (min: Int, max: Int)? {
+    if array.isEmpty { return  nil }
+    
+    var currentMin = array[0]
+    var currentMax = array[0]
+    
+    for value in array[1..<array.count]{
+        if value < currentMin {
+            currentMin = value
+        } else if value > currentMax {
+            currentMax = value
+        }
+    }
+    
+    return (currentMin, currentMax)
+}
+
+let bounds = minMax(array: [6,3,-8,3,1,9,5,15,-9])
+print("Los valores se halla entre \(bounds!.min) y \(bounds!.max)")
+
+
+func someFunction(f1 firstParamName: Int, f2 secondParamName: Int) {
+    print(firstParamName + secondParamName)
+}
+
+someFunction(f1: 2, f2: 3)
